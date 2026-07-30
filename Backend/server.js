c3bef909c70665850f71c5f685b42bed9ca2c974
Biswajit Rout <routhappy9876@gmail.com>
2026-07-30 20:11:45 +0530

    feat: add Docker configuration and serve frontend from backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ import { YSocketIO } from "y-socket.io/dist/server"
 
 
 const app = express()
+app.use(express.static("public"))
+
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
@@ -19,12 +21,7 @@ ySocketIO.initialize()
 
 
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: 'Hello World',
-        success: true
-    })
-})
+
 
 app.get("/health", (req, res) => {
     res.status(200).json({
